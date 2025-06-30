fbef69849c76a91625590728e2bfa7df7c108732
Yacine Meziani <135065821+yacinemez19@users.noreply.github.com>
2025-06-30 17:23:13 +0200

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUupovypgdxejwv8WWpwd3X4YF0ocZCDc",
-  authDomain: "agenda-rfp.firebaseapp.com",
-  projectId: "agenda-rfp",
-  storageBucket: "agenda-rfp.firebasestorage.app",
-  messagingSenderId: "324698774249",
-  appId: "1:324698774249:web:af41166bded51e6b6a2e78"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
